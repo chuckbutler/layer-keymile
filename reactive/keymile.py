4157a6af476bc5ee3e9c1bc0093c5e977558322e
Charles Butler <chuck@dasroot.net>
2016-02-04 15:16:38 +0100

    REwork to vlc/dslam charms

--- reactive/keymile.py
+++
@@ -1,6 +0,0 @@
-from charms.reactive import when, when_not
-from charmhelpers.core import fetch
-
-@when_not('keymile.downloaded')
-def download_payload():
-    fetch.install_remote('http://git.io/weave') 
