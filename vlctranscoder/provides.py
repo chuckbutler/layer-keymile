807a96fbc97be52bd763bcf7a1dc2563a3378b47
Charles Butler <chuck@dasroot.net>
2016-02-05 23:56:18 +0100

    syntax errors

--- vlctranscoder/provides.py
+++ vlctranscoder/provides.py
@@ -6,17 +6,13 @@ from charms.reactive import scopes
 
 class VLCTransocder(RelationBase):
 
-     
+     @hook('{interface:vlctranscoder}-relation-{joined,changed}')
+     def connect_to_transcoder(self):
+         set_state('transcoder.connected')
 
-    @hook('{interface:vlctranscoder}-relation-{joined,changed}')
-    def connect_to_transcoder(self):
-        set_state('transcoder.connected')
-
-    @hook('{interface:vlctranscoder}-relation-{broken,departed}')
-    def disconnect_from_transcoder(self):
-        remove_state('transcoder.connected')
+     @hook('{interface:vlctranscoder}-relation-{broken,departed}')
+     def disconnect_from_transcoder(self):
+         remove_state('transcoder.connected')
 
     def configure(self, stream_url):
         self.set_remote('stream_url', stream_url)
-
-
